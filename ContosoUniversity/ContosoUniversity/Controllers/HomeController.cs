c208a870f84a6982bb576a0cdd9094d16fe11d69
Paul <jamespauloconnor@gmail.com>
2016-04-30 22:01:31 -0400

    Added filtering in Student page and grouping in the About page.

--- ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +19,14 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +34,11 @@ namespace ContosoUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
