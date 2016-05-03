da1a0ddd7135fdfc140620b19eebda9636a75ec1
Paul <jamespauloconnor@gmail.com>
2016-05-03 01:41:49 -0400

    Updated InstructorController. Added several views.

--- ContosoUniversity/ContosoUniversity/Controllers/InstructorController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/InstructorController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
 
 namespace ContosoUniversity.Controllers
 {
@@ -16,10 +17,29 @@ namespace ContosoUniversity.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Instructor
-        public ActionResult Index()
+        public ActionResult Index(int? id, int? courseID)
         {
-            var instructors = db.Instructors.Include(i => i.OfficeAssignment);
-            return View(instructors.ToList());
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses.Select(c => c.Department))
+                .OrderBy(i => i.LastName);
+
+            if (id != null)
+            {
+                ViewBag.InstructorID = id.Value;
+                viewModel.Courses = viewModel.Instructors.Where(
+                    i => i.ID == id.Value).Single().Courses;
+            }
+
+            if (courseID != null)
+            {
+                ViewBag.CourseID = courseID.Value;
+                viewModel.Enrollments = viewModel.Courses.Where(
+                    x => x.CourseID == courseID).Single().Enrollments;
+            }
+
+            return View(viewModel);
         }
 
         // GET: Instructor/Details/5
